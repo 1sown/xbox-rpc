eed4e391e98bfdf13ed071c2771651055c41e276
Sown <114833586+1sown@users.noreply.github.com>
2024-04-19 21:20:52 +0200

    Delete xboxrpc.js

--- xboxrpc.js
+++
@@ -1,8 +0,0 @@
-const { Client, SpotifyRPC, CustomStatus, RichPresence } = require("discord.js-selfbot-v13");
-         
-const rpc = new RichPresence(client)
-        .setType('PLAYING')
-        .setApplicationId('438122941302046720')
-        .setName("Grand Theft Auto VI")
-        .setStartTimestamp(new Date());
-      client.user?.setPresence({ activities: [rpc] });
