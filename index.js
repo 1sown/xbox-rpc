0af2d57996eef50c78e7efb9c11e29a66e5d61cc
Sans <002notsans@gmail.com>
2024-04-28 00:53:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { Client, SpotifyRPC, CustomStatus, RichPresence } = require("discord.js-selfbot-v13");
-const config = require('./config')        
+const { Client, RichPresence } = require("discord.js-selfbot-v13");
+const { token, status} = require('./config');
 
-const client = new Client({})
-client.login(config.token)
+const client = new Client()
+client.login(token)
 
 client.on('ready', () => {
-    console.log(client.user.username)
+    console.log(`${client.user.username} est connecté`)
     const rpc = new RichPresence(client)
-        .setType('PLAYING')
+        .setType(0)
         .setApplicationId('438122941302046720')
-        .setName(config.status)
+        .setName(status)
         .setStartTimestamp(new Date());
-      client.user?.setPresence({ activities: [rpc] });
+    client.user.setPresence({ activities: [rpc] });
 })
